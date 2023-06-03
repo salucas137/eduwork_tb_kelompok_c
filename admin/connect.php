c8dc7d68b1fedeb5c29a1eb6a79dffc957386497
anggraininova <105601196+anggraininova@users.noreply.github.com>
2023-06-03 11:07:51 +0700

    Update connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -2,7 +2,7 @@
 $hostName="localhost";
 $username="root";
 $password="";
-$dbName ="film_db";
+$dbName ="db_film";
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $conn = mysqli_connect($hostName,$username,$password,$dbName);
