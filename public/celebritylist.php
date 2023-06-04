06aafb0024dbac205a26e653f04a7f2d7446f65c
teguhadt <teguhaditia298@gmail.com>
2023-06-05 00:10:45 +0700

    update celebritylist.php

--- public/celebritylist.php
+++ public/celebritylist.php
@@ -127,6 +127,17 @@
 ?>
 					
 					<p class="pad-change">Found <span><?php echo $data; ?> celebrities</span> in total</p>
+					<label>Sort by:</label>
+					<select>
+						<option value="popularity">Popularity Descending</option>
+						<option value="popularity">Popularity Ascending</option>
+						<option value="rating">Rating Descending</option>
+						<option value="rating">Rating Ascending</option>
+						<option value="date">Release date Descending</option>
+						<option value="date">Release date Ascending</option>
+					</select>
+					<a href="#" class="list"><i class="ion-ios-list-outline "></i></a>
+					<a  href="celebritylist.php" class="grid"><i class="ion-grid active"></i></a>
 
 				</div>
 
@@ -139,9 +150,9 @@
           ?>
 
 					<div class="ceb-item">
-						<a href="celebrityes.php?id=<?php echo $data['id']; ?>"><img src="<?php echo $data["foto"]; ?>" alt="" width="270" height="400"></a>
+						<a href="celebrity.php?celebrityid=<?= $data['id'] ?>"><img style="width: 270px; height: auto;" src="<?php echo $data["foto"]; ?>" alt="" ></a>
 						<div class="ceb-infor">
-							<h2><a href="celebrityes.php?id=<?php echo $data['id']; ?>"><?php echo $data['nama_pemain'] ?></a></h2>
+							<h2><a href="celebrity.php?celebrityid=<?= $data['id'] ?>"><?php echo $data['nama_pemain'] ?></a></h2>
 							<span>Actor, <?php echo $data['negara'] ?></span>
 						</div>
 					</div>
@@ -173,56 +184,10 @@
 			</div>
 			<div class="col-md-3 col-xs-12 col-sm-12">
 				<div class="sidebar">
-						<div class="searh-form">
-						<h4 class="sb-title">Search celebrity</h4>
-						<form class="form-style-1 celebrity-form" action="#">
-							<div class="row">
-								<div class="col-md-12 form-it">
-									<label>Celebrity name</label>
-									<input type="text" placeholder="Enter keywords">
-								</div>
-								<div class="col-md-12 form-it">
-									<label>Celebrity Letter</label>
-									<select>
-									  <option value="range">A</option>
-									  <option value="saab">B</option>
-									</select>
-								</div>
-								<div class="col-md-12 form-it">
-									<label>Category</label>
-									<select>
-									  <option value="range">Actress</option>
-									  <option value="saab">Others</option>
-									</select>
-								</div>
-								<div class="col-md-12 form-it">
-									<label>Year of birth</label>
-									<div class="row">
-										<div class="col-md-6">
-											<select>
-											  <option value="range">1970</option>
-											  <option value="number">Other</option>
-											</select>
-										</div>
-										<div class="col-md-6">
-											<select>
-											  <option value="range">1990</option>
-											  <option value="number">others</option>
-											</select>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-12 ">
-									<input class="submit" type="submit" value="submit">
-								</div>
-							</div>
-						</form>
-					</div>
-					<div class="ads">
-						<img src="images/uploads/ads1.png" alt="">
-					</div>
+						
+					
 					<div class="celebrities">
-						<h4 class="sb-title">featured celebrity</h4>
+						<h4 class="sb-title">SPOTLIGHT CELEBRITIES</h4>
 						<?php
 						$limit = 4; 
                     $offsetQuery = "";
