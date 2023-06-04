f61ed3b3a01485680e0f76dc877e109850259dc6
teguhadt <teguhaditia298@gmail.com>
2023-06-05 00:11:21 +0700

    added about.php

---
+++ public/about.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7 no-js" lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8 no-js" lang="en-US">
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html lang="en" class="no-js">
+
+<!-- landing14:04-->
+<head>
+	<!-- Basic need -->
+	<title>About</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<link rel="profile" href="#">
+
+    <!--Google Font-->
+    <link rel="stylesheet" href='http://fonts.googleapis.com/css?family=Dosis:400,700,500|Nunito:300,400,600' />
+	<!-- Mobile specific meta -->
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<meta name="format-detection" content="telephone-no">
+
+	<!-- CSS files -->
+	<link rel="stylesheet" href="../css/plugins.css">
+	<link rel="stylesheet" href="../css/style.css">
+
+</head>
+<body>
+<!--preloading-->
+<div id="preloader">
+    <img class="logo" src="../images/logo.png" alt="" width="119" height="58">
+    <div id="status">
+        <span></span>
+        <span></span>
+    </div>
+</div>
+<!--end of preloading-->
+<!--login form popup-->
+<!--end of login form popup-->
+<!--signup form popup-->
+<!--end of signup form popup-->
+
+	<!-- BEGIN | Header -->
+	<header class="ht-header">
+		<div class="container">
+			<nav class="navbar navbar-default navbar-custom">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header logo">
+					<div class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<div id="nav-icon1">
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+					</div>
+					<a href="index.php"><img class="logo" src="../images/logo.png" alt="" width="240" height="58"></a>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse flex-parent" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav flex-child-menu menu-left">
+						<li class="hidden">
+							<a href="#page-top"></a>
+						</li>
+						<li>
+							<a href="index.php" class="btn btn-default">
+								Home
+							</a>
+						</li>
+						<li>
+							<a href="movielist.php" class="btn btn-default">
+								movies</i>
+							</a>
+						</li>
+						<li>
+							<a href="celebritylist.php" class="btn btn-default">
+								Celebrities</i>
+							</a>
+						</li>
+						<li>
+							<a href="about.php" class="btn btn-default">
+								About</i>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<!-- /.navbar-collapse -->
+			</nav>
+
+			<!-- top search form -->
+		</div>
+	</header>
+	<!-- END | Header -->
+
+<div class="landing-hero">
+	<img src="../images/logo.png" alt="Logo">
+	<div class="row landing-hero-text">
+		<h2>Movie Verse</h2>
+		<h2 class="text-yellow">FILM REVIEW  &middot;  MOVIE TOP</h2>
+		<h2>Kassalman - Teguh - Ievy - Nova</h2>
+	</div>
+	<a href="#" class="redbtn" id="discover-now">DISCOVER NOW</a>
+</div>
+
+<!-- footer v3 section-->
+<div class="ft-copyright-landing">
+	<p><a target="_blank" href="t">Dibuat oleh: Kelompok C</a></p>
+</div>
+<!-- end of footer v3 section-->
+
+<script src="../js/jquery.js"></script>
+<script src="../js/plugins.js"></script>
+<script src="../js/plugins2.js"></script>
+<script src="../js/custom.js"></script>
+</body>
+
+<!-- landing14:38-->
+</html>
