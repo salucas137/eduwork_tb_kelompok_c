7ee638a9839cea488f9e2ff799ef1c4697d73501
Kassalman Muhammad <121538293+salucas137@users.noreply.github.com>
2023-06-05 19:43:20 +0700

    Add files via upload

--- public/movielist.php
+++ public/movielist.php
@@ -119,20 +119,7 @@ $query = mysqli_query($conn, "SELECT * FROM `film`");
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12 col-sm-12 col-xs-12">
-					<div class="topbar-filter fw">
-						<p>Found <span>1,608 movies</span> in total</p>
-						<label>Sort by:</label>
-						<select>
-							<option value="popularity">Popularity Descending</option>
-							<option value="popularity">Popularity Ascending</option>
-							<option value="rating">Rating Descending</option>
-							<option value="rating">Rating Ascending</option>
-							<option value="date">Release date Descending</option>
-							<option value="date">Release date Ascending</option>
-						</select>
-						<a href="movielist.html" class="list"><i class="ion-ios-list-outline "></i></a>
-						<a href="moviegridfw.html" class="grid"><i class="ion-grid active"></i></a>
-					</div>
+					
                     
 					<div class="flex-wrap-movielist mv-grid-fw">
                     <?php
@@ -150,24 +137,7 @@ $query = mysqli_query($conn, "SELECT * FROM `film`");
                         <?php endwhile ?>
 					</div>
                     
-					<div class="topbar-filter">
-						<label>Movies per page:</label>
-						<select>
-							<option value="range">20 Movies</option>
-							<option value="saab">10 Movies</option>
-						</select>
-
-						<div class="pagination2">
-							<span>Page 1 of 2:</span>
-							<a class="active" href="#">1</a>
-							<a href="#">2</a>
-							<a href="#">3</a>
-							<a href="#">...</a>
-							<a href="#">78</a>
-							<a href="#">79</a>
-							<a href="#"><i class="ion-arrow-right-b"></i></a>
-						</div>
-					</div>
+					
 				</div>
 			</div>
 		</div>
