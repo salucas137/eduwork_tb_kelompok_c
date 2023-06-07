fcd6943a4e265cd4c3d132be67ee53051997ed4a
anggraininova <105601196+anggraininova@users.noreply.github.com>
2023-06-07 22:12:47 +0700

    Update film.php

--- admin/film.php
+++ admin/film.php
@@ -44,7 +44,7 @@
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     <!-- MyCSS -->
-    <link rel="stylesheet" href="admin/style.css">
+    <link rel="stylesheet" href="style.css">
     
     <link rel="stylesheet" href="assets/css/main/app.css">
     <link rel="stylesheet" href="assets/css/main/app-dark.css">
