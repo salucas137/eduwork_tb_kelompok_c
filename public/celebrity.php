46df84ebebee66fe07b22274a3afa3c65ef0996e
Kassalman Muhammad <121538293+salucas137@users.noreply.github.com>
2023-06-15 18:26:31 +0700

    add login

--- public/celebrity.php
+++ public/celebrity.php
@@ -40,17 +40,96 @@ $query = mysqli_query($conn, "SELECT * FROM `pemain` where pemain.id_pemain='$ce
 
 <body>
 	<!--preloading-->
-	<div id="preloader">
-		<img class="logo" src="../images/logo.png" alt="" width="240" height="">
-		<div id="status">
-			<span></span>
-			<span></span>
-		</div>
-	</div>
-	<!--end of preloading-->
+<div id="preloader">
+    <img class="logo" src="../images/logo.png" alt="" width="240" height="">
+    <div id="status">
+        <span></span>
+        <span></span>
+    </div>
+</div>
+<!--end of preloading-->
+<!--login form popup-->
+<div class="login-wrapper" id="login-content">
+    <div class="login-content">
+        <a href="#" class="close">x</a>
+        <h3>Login</h3>
+        <form method="post" action="#">
+        	<div class="row">
+        		 <label for="username">
+                    Username:
+                    <input type="text" name="username" id="username" placeholder="Hugh Jackman" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$" required="required" />
+                </label>
+        	</div>
+           
+            <div class="row">
+            	<label for="password">
+                    Password:
+                    <input type="password" name="password" id="password" placeholder="******" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
+                </label>
+            </div>
+            <div class="row">
+            	<div class="remember">
+					<div>
+						<input type="checkbox" name="remember" value="Remember me"><span>Remember me</span>
+					</div>
+            		<a href="#">Forget password ?</a>
+            	</div>
+            </div>
+           <div class="row">
+           	 <button type="submit">Login</button>
+           </div>
+        </form>
+        <div class="row">
+        	<p>Or via social</p>
+            <div class="social-btn-2">
+            	<a class="fb" href="#"><i class="ion-social-facebook"></i>Facebook</a>
+            	<a class="tw" href="#"><i class="ion-social-twitter"></i>twitter</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!--end of login form popup-->
+<!--signup form popup-->
+<div class="login-wrapper"  id="signup-content">
+    <div class="login-content">
+        <a href="#" class="close">x</a>
+        <h3>sign up</h3>
+        <form method="post" action="#">
+            <div class="row">
+                 <label for="username-2">
+                    Username:
+                    <input type="text" name="username" id="username-2" placeholder="Hugh Jackman" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$" required="required" />
+                </label>
+            </div>
+           
+            <div class="row">
+                <label for="email-2">
+                    your email:
+                    <input type="password" name="email" id="email-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
+                </label>
+            </div>
+             <div class="row">
+                <label for="password-2">
+                    Password:
+                    <input type="password" name="password" id="password-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
+                </label>
+            </div>
+             <div class="row">
+                <label for="repassword-2">
+                    re-type Password:
+                    <input type="password" name="password" id="repassword-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
+                </label>
+            </div>
+           <div class="row">
+             <button type="submit">sign up</button>
+           </div>
+        </form>
+    </div>
+</div>
+<!--end of signup form popup-->
 
-	<!-- BEGIN | Header -->
-	<header class="ht-header">
+<!-- BEGIN | Header -->
+<header class="ht-header">
 		<div class="container">
 			<nav class="navbar navbar-default navbar-custom">
 				<!-- Brand and toggle get grouped for better mobile display -->
@@ -77,41 +156,45 @@ $query = mysqli_query($conn, "SELECT * FROM `pemain` where pemain.id_pemain='$ce
 						</li>
 						<li class="dropdown first">
 							<a class="btn btn-default" href="movielist.php">
-								movies
+								Movies
 							</a>
 						</li>
-						<li>
-							<a href="category.php" class="btn btn-default">
-								Categories</i>
+						<li class="dropdown first">
+							<a class="btn btn-default" href="category.php">
+								Category
 							</a>
 						</li>
 						<li class="dropdown first">
 							<a class="btn btn-default" href="celebritylist.php">
-								celebrities
+								Celebrity
 							</a>
 						</li>
 						<li class="dropdown first">
 							<a class="btn btn-default" href="about.php">
-								about
+								About
 							</a>
 						</li>
 
 					</ul>
+					<ul class="nav navbar-nav flex-child-menu menu-right">
+						<li class="loginLink"><a href="#">LOG In</a></li>
+						<li class="btn signupLink"><a href="#">sign up</a></li>
+					</ul>
 				</div>
 				<!-- /.navbar-collapse -->
 			</nav>
-
-			<!-- top search form -->
-			<!--<div class="top-search">
-				<select>
-					<option value="united">Movie</option>
-					<option value="saab">Others</option>
-				</select>
-				<input type="text" placeholder="Search for a movie, TV Show or celebrity that you are looking for">
-			</div>-->
-		</div>
-	</header>
-	<!-- END | Header -->
+	    
+	    <!-- top search form -->
+	    <!--<div class="top-search">
+	    	<select>
+				<option value="united">TV show</option>
+				<option value="saab">Others</option>
+			</select>
+			<input type="text" placeholder="Search for a movie, TV Show or celebrity that you are looking for">
+	    </div>-->
+	</div>
+</header>
+<!-- END | Header -->
 
 	<div class="hero hero3">
 		<div class="container">
