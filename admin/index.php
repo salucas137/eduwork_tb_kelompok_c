d38c8954303b26aee507b316343677cc7bebdbf6
anggraininova <105601196+anggraininova@users.noreply.github.com>
2023-06-19 17:33:53 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,6 @@
   $jumlah_komentar = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT count(id_komentar) as jumlah_komentar FROM komentar"));
   $jumlah_komentar = $jumlah_komentar['jumlah_komentar'];
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,8 +20,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    
+    <title>Admin Dashboard</title>
+
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap 4 -->
@@ -39,15 +38,11 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     <!-- MyCSS -->
     <link rel="stylesheet" href="assets/css/style.css">
-
+    
     <link rel="stylesheet" href="assets/css/main/app.css">
-    <link rel="stylesheet" href="assets/css/main/app-dark.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    
     <link rel="shortcut icon" href="assets/images/logo/favicon.svg" type="image/x-icon">
     <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/png">
-    
-<link rel="stylesheet" href="assets/css/shared/iconly.css">
-
 </head>
 
 <body>
@@ -57,7 +52,7 @@
     <div class="sidebar-header position-relative">
         <div class="d-flex justify-content-between align-items-center">
             <div class="logo">
-                <a href="dashboard.php"><img src="assets/images/logo/logo.png" alt="Logo" srcset="" width="130"></a>
+                <a href="index.php"><img src="assets/images/logo/logo.png" alt="Logo" srcset="" width="130"></a>
             </div>
             <div class="theme-toggle d-flex gap-2  align-items-center mt-2">
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--system-uicons" width="20" height="20" preserveAspectRatio="xMidYMid meet" viewBox="0 0 21 21"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path d="M10.5 14.5c2.219 0 4-1.763 4-3.982a4.003 4.003 0 0 0-4-4.018c-2.219 0-4 1.781-4 4c0 2.219 1.781 4 4 4zM4.136 4.136L5.55 5.55m9.9 9.9l1.414 1.414M1.5 10.5h2m14 0h2M4.135 16.863L5.55 15.45m9.899-9.9l1.414-1.415M10.5 19.5v-2m0-14v-2" opacity=".3"></path><g transform="translate(-210 -1)"><path d="M220.5 2.5v2m6.5.5l-1.5 1.5"></path><circle cx="220.5" cy="11.5" r="4"></circle><path d="m214 5l1.5 1.5m5 14v-2m6.5-.5l-1.5-1.5M214 18l1.5-1.5m-4-5h2m14 0h2"></path></g></g></svg>
@@ -77,13 +72,13 @@
             <li class="sidebar-title">Menu</li>
             
             <li
-                class="sidebar-item active ">
-                <a href="dashboard.php" class='sidebar-link'>
+                class="sidebar-item active">
+                <a href="index.php" class='sidebar-link'>
                     <i class="bi bi-grid-fill"></i>
                     <span>Dashboard</span>
                 </a>
             </li>
-            
+
             <li
                 class="sidebar-item  ">
                 <a href="film.php" class='sidebar-link'>
@@ -124,74 +119,123 @@
                     <span>Logout</span>
                 </a>
             </li>
+            
         </ul>
     </div>
 </div>
         </div>
-        <div id="main">
-            <header class="mb-3">
-                <a href="#" class="burger-btn d-block d-xl-none">
-                    <i class="bi bi-justify fs-3"></i>
-                </a>
+        <div id="main" class='layout-navbar'>
+            <header class='mb-3'>
+                <nav class="navbar navbar-expand navbar-light navbar-top">
+                    <div class="container-fluid">
+                        <a href="#" class="burger-btn d-block">
+                            <i class="bi bi-justify fs-3"></i>
+                        </a>
+
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mb-lg-0">
+                            </ul>
+                            <div class="dropdown">
+                                <a href="login.php" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <div class="user-menu d-flex">
+                                        <div class="user-name text-end me-3">
+                                            <h6 class="mb-0 text-gray-600">Admin</h6>
+                                            <p class="mb-0 text-sm text-gray-600">Administrator</p>
+                                        </div>
+                                        <div class="user-img d-flex align-items-center">
+                                            <div class="avatar avatar-md">
+                                                <img src="assets/images/faces/1.jpg">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton" style="min-width: 11rem;">
+                                    <li>
+                                        <h6 class="dropdown-header">Hello, Admin!</h6>
+                                    </li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="logout.php"><i
+                                                class="icon-mid bi bi-box-arrow-left me-2"></i> Logout</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
             </header>
-            
-    <div class="page-heading">
-        <h3>Dashboard</h3>
-    </div>
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row my-2">
-          <div class="col-lg-3">
-            <div class="card">
-              <div class="card-body">
-                <h5><i class="fas fa-fw fa-theater-masks"></i> Genre Film</h5>
-                <h6 class="mb-2 text-muted">Jumlah Genre: <?= $jumlah_genre; ?></h6>
-                <a href="kategori.php" class="card-link btn btn-primary"><i class="fas fa-fw fa-align-justify"></i></a>
-              </div>
+            <div id="main-content">
+                
+<div class="page-heading">
+    <div class="page-title">
+        <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+                <h3>Dashboard</h3>
             </div>
-          </div>
-
-          <div class="col-lg-3">
-            <div class="card">
-              <div class="card-body">
-                <h5><i class="fas fa-fw fa-film"></i> Film</h5>
-                <h6 class="mb-2 text-muted">Jumlah Film: <?= $jumlah_film; ?></h6>
-                <a href="film.php" class="card-link btn btn-primary"><i class="fas fa-fw fa-align-justify"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3">
-            <div class="card">
-              <div class="card-body">
-                <h5><i class="bi bi-people-fill"></i> Pemain</h5>
-                <h6 class="mb-2 text-muted">Jumlah Pemain: <?= $jumlah_pemain; ?></h6>
-                <a href="pemain.php" class="card-link btn btn-primary"><i class="fas fa-fw fa-align-justify"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3">
-            <div class="card">
-              <div class="card-body">
-                <h5><i class="fas fa-fw fa-comment"></i> Komentar</h5>
-                <h6 class="mb-2 text-muted">Jumlah Komentar: <?= $jumlah_komentar; ?></h6>
-                <a href="komentar.php" class="card-link btn btn-primary"><i class="fas fa-fw fa-align-justify"></i></a>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
+    </div>
+    <section class="section">
+        <div class="container-fluid">
+            <div class="row my-2">
+                <div class="col-lg-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5><i class="fas fa-fw fa-theater-masks"></i> Genre Film</h5>
+                            <h6 class="mb-2 text-muted">Jumlah Genre: <?= $jumlah_genre; ?></h6>
+                            <a href="kategori.php" class="card-link btn btn-primary"><i class="fas fa-fw fa-align-justify"></i></a>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="col-lg-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5><i class="fas fa-fw fa-film"></i> Film</h5>
+                            <h6 class="mb-2 text-muted">Jumlah Film: <?= $jumlah_film; ?></h6>
+                            <a href="film.php" class="card-link btn btn-primary"><i class="fas fa-fw fa-align-justify"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5><i class="bi bi-people-fill"></i> Pemain</h5>
+                            <h6 class="mb-2 text-muted">Jumlah Pemain: <?= $jumlah_pemain; ?></h6>
+                            <a href="pemain.php" class="card-link btn btn-primary"><i class="fas fa-fw fa-align-justify"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5><i class="fas fa-fw fa-comment"></i> Komentar</h5>
+                            <h6 class="mb-2 text-muted">Jumlah Komentar: <?= $jumlah_komentar; ?></h6>
+                            <a href="komentar.php" class="card-link btn btn-primary"><i class="fas fa-fw fa-align-justify"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
     </section>
+</div>
+
 
     <script src="assets/js/bootstrap.js"></script>
     <script src="assets/js/app.js"></script>
     <script src="assets/js/main.js"></script>
 
-<!-- Need: Apexcharts -->
-<script src="assets/extensions/apexcharts/apexcharts.min.js"></script>
-<script src="assets/js/pages/dashboard.js"></script>
-
+    <!-- Need: Apexcharts -->
+    <script src="assets/extensions/apexcharts/apexcharts.min.js"></script>
+    <script src="assets/js/pages/dashboard.js"></script>
+    
 </body>
 
 </html>
