6ac5e91f5a03f9cb9e9d32bb11bf55f1868836b1
anggraininova <105601196+anggraininova@users.noreply.github.com>
2023-06-21 15:16:26 +0700

    Update kategori.php

--- admin/kategori.php
+++ admin/kategori.php
@@ -116,7 +116,13 @@
                     <span>Komentar</span>
                 </a>
             </li>
-            
+            <li
+                class="sidebar-item active">
+                <a href="user.php" class='sidebar-link'>
+                    <i class="bi bi-people"></i>
+                    <span>Users</span>
+                </a>
+            </li>
             <li
                 class="sidebar-item  ">
                 <a href="logout.php" class='sidebar-link'>
